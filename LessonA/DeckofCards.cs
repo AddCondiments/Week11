9a13492eb1b7cfc526e5b8cd3912ca2519e64131
AddCondiments <addcondiments@gmail.com>
2017-07-25 15:33:34 -0400

    Added the DeckofCards class.

---
+++ LessonA/DeckofCards.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LessonA
+{
+    /* Matthew Watada
+     * July 25th 2017
+     * This is some kind of magic, we turnede a class into a list.
+     * A class that inherits from the list collection, and creates a list of type Card.
+     * Version 0.3
+     */
+    public class DeckofCards:List<Card>
+    {
+        // Private Instance Variables
+
+        // Public Properties
+
+        // Constructors
+
+        // Private Methods
+
+        // Public Methods
+    }
+}
